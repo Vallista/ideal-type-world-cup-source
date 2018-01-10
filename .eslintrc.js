322fc029927fc23c13c9f80f44a055ed38df9357
마광휘 <vallista@tistory.com>
2018-01-10 17:16:47 +0900

    레이아웃 적용 및 트리 작성 전

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "indent": [
+            "error",
+            4,
+        ],
+        "max-len": [
+            "error",
+            {
+                "code": 10000,
+            },
+        ],
+    },
+};
